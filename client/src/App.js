d29f667459c40cfc99b3e2abe88a2b3003397da2
gautam <gbpractice2000@gmail.com>
2022-12-26 18:46:35 +0530

    Aios commit 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
+import {BrowserRouter as Router ,Routes,Route, Link} from "react-router-dom"
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
     <>
+    <Router>
       <header>
-        <a href="/">AIOS</a>
+        <Link to="/">AIOS</Link>
       </header>
       <main>
-        Products
+      <Routes>
+        <Route path='/' element={<HomeScreen />}></Route>
+        <Route path='/product/:slug' element={<ProductScreen />}></Route>
+      </Routes>
+     
       </main>
+      </Router>
     </>
   );
 }
---
+++ client/src/screens/HomeScreen.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import data from '../data';
+import '../App.css';
+import { Link } from 'react-router-dom';
+
+const HomeScreen = () => {
+  return (
+    <>
+           <h1>Fearured products</h1>
+
+<div className="products">
+  {
+    data.products.map(product => (
+      <div className="product" key={product.slug}>
+        <Link to={`/product/${product.slug}`}>
+          <img src={product.image} alt={product.name} />
+        </Link>
+        <div className="product-info">
+          <Link to={`/product/${product.slug}`}>
+            <p>{product.name}</p>
+          </Link>
+          <p>
+            <strong>${product.price}</strong>
+          </p>
+          <button>Add to cart</button>
+        </div>
+      </div>))
+  }
+</div>
+    </>
+  )
+}
+
+export default HomeScreen
