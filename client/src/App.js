4100aabe931f69beab87aaf858d1203d9093d2fe
gautam <gbpractice2000@gmail.com>
2022-12-26 11:01:34 +0530

    Aios first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header>
+        <a href="/">AIOS</a>
+      </header>
+      <main>
+        Products
+      </main>
+    </>
+  );
+}
+
+export default App;
